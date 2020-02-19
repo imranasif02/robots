a0c20070b3be5efd585a324e72749967877d732c
unknown <imranasif161@gmail.com>
2020-02-19 23:13:52 +0300

    Updates

---
+++ precache-manifest.2d1c0a21d5f6d6c38c7fecf95ee072ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bbb090dd16772b420117005ea1e33f8",
+    "url": "/Monstor-robot/index.html"
+  },
+  {
+    "revision": "26fc8fc44f74d12940ec",
+    "url": "/Monstor-robot/static/css/main.5ab94e63.chunk.css"
+  },
+  {
+    "revision": "e4fdf7b00b3ffb05351d",
+    "url": "/Monstor-robot/static/js/2.ac8e234b.chunk.js"
+  },
+  {
+    "revision": "7fc60fe721fa046663bd312b1d917655",
+    "url": "/Monstor-robot/static/js/2.ac8e234b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "26fc8fc44f74d12940ec",
+    "url": "/Monstor-robot/static/js/main.ec570c83.chunk.js"
+  },
+  {
+    "revision": "6b0ca0f6234461c3306d",
+    "url": "/Monstor-robot/static/js/runtime-main.0d2904e8.js"
+  }
+]);
